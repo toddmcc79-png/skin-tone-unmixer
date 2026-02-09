336aea44fb20f06f886b2cdab84f93abd9a745b3
toddmcc79-png <toddmcc79@gmail.com>
2026-02-08 20:53:28 -0600

    Adjust path screen button positions 7

--- src/screensConfig.js
+++ src/screensConfig.js
@@ -11,9 +11,9 @@ export const screens = {
   path: {
     background: "/screens/path.png",
     hotspots: [
-      { id: "a", x: 5, y: 57, w: 28, h: 20, to: "/a/1" },
-      { id: "b", x: 37, y: 57, w: 28, h: 20, to: "/b/1" },
-      { id: "c", x: 67, y: 57, w: 28, h: 20, to: "/c/1" },
+      { id: "a", x: 5, y: 40, w: 28, h: 25, to: "/a/1" },
+      { id: "b", x: 34, y: 40, w: 28, h: 25, to: "/b/1" },
+      { id: "c", x: 66, y: 40, w: 28, h: 25, to: "/c/1" },
     ],
   },
 
